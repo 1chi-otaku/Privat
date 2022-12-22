c6362dfbb74ab14aa8bcc5c80fbe1cd947558765
Serhii <mywaifuqueen@gmail.com>
2022-12-22 13:17:22 +0000

    1;17

--- HttpGetHtmlPAge/HttpGetHtmlPAge.cpp
+++ HttpGetHtmlPAge/HttpGetHtmlPAge.cpp
@@ -7,12 +7,16 @@ using namespace std;
 
 int main()
 {
+    const int DATA_AMOUNT = 2;
     // the URL to download from
     const char* privatURL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
     const char* bankgovuaURL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
     // the destination file
     const char* destFilePrivat = "privat.txt";
     const char* destFileBankGovUa = "BankGovUa.txt";
+    string preparcingPrivat, postparcingPrivat, preparcingBankGovUa, postparcingGovBankUa, buffer;
+    string subject_to_search[2]{ "EUR", "USD" };
+    string subject_to_search2[2]{ "\"cc\":\"EUR\"", "\"cc\":\"USD\"" };
 
     // URLDownloadToFile returns S_OK on success
     if (S_OK == URLDownloadToFileA(NULL, privatURL, destFilePrivat, 0, NULL))
@@ -24,9 +28,7 @@ int main()
         cout << "Saved to " << destFileBankGovUa << "\n";
     }
 
-    string preparcingPrivat,preparcingBankGovUa, buffer;
-
-    std::ifstream ingov("BankGovUa.txt");
+    ifstream ingov("BankGovUa.txt");
     if (ingov.is_open())
     {
         while (getline(ingov, buffer))
@@ -36,35 +38,23 @@ int main()
     }
     ingov.close();
 
-    std::ifstream in("privat.txt");
+    ifstream in("privat.txt");
     if (in.is_open())
     {
-        while (getline(in, preparcingPrivat))
+        while (getline(in, buffer))
         {
-            
+            preparcingPrivat += buffer;
         }
     }
     in.close();
-
-   
     cout << endl;
     
-    for (int i = 0; i < preparcingPrivat.length(); i++)
-    {
-        if (preparcingPrivat[i + 1] == ',' || preparcingPrivat[i] == '{' || preparcingPrivat[i + 1] == '}') {
-            preparcingPrivat.insert(i + 1, 1, '\n');
-            i++;
-        }
-    }   
-    string subject_to_search[2] { "EUR", "USD" };
-    string postparcingPrivat;
-
     //Privat Parcing
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < DATA_AMOUNT; i++)
     {
         size_t found = preparcingPrivat.find(subject_to_search[i]);
         postparcingPrivat += subject_to_search[i] + " - ";
-        for (int j = found + subject_to_search[i].length() + 27; preparcingPrivat[j] != '\n'; j++)
+        for (int j = found + 29; preparcingPrivat[j] != '"'; j++)
         {
             if (preparcingPrivat[j] != '"') {
                 postparcingPrivat += preparcingPrivat[j];
@@ -72,14 +62,9 @@ int main()
         }
         postparcingPrivat += '\n';
     }
-    cout << "Privat Bank data:" << endl;
-    cout << postparcingPrivat;
-
-    string subject_to_search2[2]{"\"cc\":\"EUR\"", "\"cc\":\"USD\"" };
-    string postparcingGovBankUa;
 
     //BankGovUa Parcing
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < DATA_AMOUNT; i++)
     {
         size_t found = preparcingBankGovUa.find(subject_to_search2[i]);
         postparcingGovBankUa += subject_to_search[i] + " - ";
@@ -91,8 +76,9 @@ int main()
         }
         postparcingGovBankUa += '\n';
     }
+    cout << "Privat Bank data:" << endl;
+    cout << postparcingPrivat << endl;
     cout << "Bank Gov Ua data:" << endl;
-    cout <<  postparcingGovBankUa;
-    
-
+    cout << postparcingGovBankUa << endl;
+    return 0;
 }
